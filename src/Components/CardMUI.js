4c0b07c16ad0d8ca9a91cdad1695c7503a305a97
Jacklau1216 <mklauag@connect.ust.hk>
2023-06-03 22:58:36 +0200

    bar chart and leaderboard

--- src/Components/CardMUI.js
+++ src/Components/CardMUI.js
@@ -1,15 +1,26 @@
 import { Card, CardContent, CardActionArea, List, ListItem, ListItemIcon, ListItemText, Typography } from '@mui/material';
+import { useEffect } from 'react';
+import * as React from "react";
+
 
 export default function CardMUI (props) {
   const title = props.title;
-  const top1 = props.top1;
-  const top2 = props.top2;
-  const top3 = props.top1;
+  const [top1, settop1] = React.useState(undefined);
+  const [top2, settop2] = React.useState(undefined);
+  const [top3, settop3] = React.useState(undefined);
+
+
+  useEffect(()=>{
+    settop1(props.data[0])
+    settop2(props.data[1])
+    settop3(props.data[2])
+  }, [props.data])
+
   return (
     <Card sx={{ maxWidth: 345, gap: 2, borderRadius: 2 }}>
       <CardActionArea>
         <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
+          <Typography gutterBottom variant="h4" component="div">
             {title}
           </Typography>
           <List>
@@ -18,8 +29,8 @@ export default function CardMUI (props) {
                 <img className="card-rank-icon" alt="rank 1 icon" src={process.env.PUBLIC_URL + "/images/rank1.png"} />
               </ListItemIcon>
               <ListItemText
-                primary='fuck!!!'
-                secondary='Secondary text'
+                primary={top1 !== undefined ? top1.id : 'loading'}
+                secondary={top1 !== undefined ? top1.player : 'loading'}
               />
             </ListItem>
             <ListItem>
@@ -27,8 +38,8 @@ export default function CardMUI (props) {
                 <img className="card-rank-icon" alt="rank 2 icon" src={process.env.PUBLIC_URL + "/images/rank2.png"} />
               </ListItemIcon>
               <ListItemText
-                primary="Single-line item"
-                secondary='Secondary text'
+                primary={top2 !== undefined ? top2.id : 'loading'}
+                secondary={top2 !== undefined ? top2.player : 'loading'}
               />
             </ListItem>
             <ListItem>
@@ -36,8 +47,8 @@ export default function CardMUI (props) {
                 <img className="card-rank-icon" alt="rank 3 icon" src={process.env.PUBLIC_URL + "/images/rank3.png"} />
               </ListItemIcon>
               <ListItemText
-                primary="Single-line item"
-                secondary='Secondary text'
+                primary={top3 !== undefined ? top3.id : 'loading'}
+                secondary={top3 !== undefined ? top3.player : 'loading'}
               />
             </ListItem>
           </List>
--- src/Components/Chart.js
+++ src/Components/Chart.js
@@ -1,179 +1,193 @@
-import './Chart.css';
+import "./Chart.css";
 
-import { useRef, useEffect, useState } from 'react';
-import * as React from 'react';
-import * as d3 from 'd3';
+import { useRef, useEffect, useState } from "react";
+import * as React from "react";
+import * as d3 from "d3";
 
 //MUI
-import Slider from '@mui/material/Slider';
-import { styled } from '@mui/material/styles';
-import { Container, Box, Button, Stack, ToggleButton } from '@mui/material';
-import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
-import { element } from 'prop-types';
+import Slider from "@mui/material/Slider";
+import { styled } from "@mui/material/styles";
+import { Container, Box, Button, Stack, ToggleButton } from "@mui/material";
+import ToggleButtonGroup from "@mui/material/ToggleButtonGroup";
+import { element } from "prop-types";
 
 export default function Chart(props) {
-    const divRef = useRef(null);
-    const datadict = useRef(props.data)
-    const Xaxis = useRef(null)
-    const Yaxis = useRef(null)
-    const color = useRef({})
+  const divRef = useRef(null);
+  const datadict = useRef(props.data);
+  const Xaxis = useRef(null);
+  const Yaxis = useRef(null);
+  const color = useRef({});
+  const change = useRef(false);
 
-    
-    //* update function
-    function update(data) {
-        var margin = {top: 50, right: 30, bottom: 50, left: 110},
+  //* update function
+  function update(data) {
+    var margin = { top: 50, right: 30, bottom: 50, left: 110 },
+      width = test.width * 0.8,
+      height = test.height * 0.9;
+
+    var temp = data.sort(function (b, a) {
+      return a.value - b.value;
+    });
+
+    var xmax = d3.max(data, function (d) {
+      return d.value;
+    });
+
+    var svg = d3.select(divRef.current);
+    var u = svg.selectAll("rect").data(temp);
+
+    // X axis
+    var x = d3.scaleLinear().domain([0, 10000]).range([0, width]);
+
+    // Y axis
+    var y = d3
+      .scaleBand()
+      .domain(props.data.map((d) => d.group))
+      .range([0, height])
+      .padding(0.1);
+
+    x.domain([0, xmax]);
+    Xaxis.current
+      .transition()
+      .duration(1000)
+      .call(d3.axisBottom(x))
+      .selectAll("text")
+      .attr("transform", "translate(-10,0)rotate(-45)")
+      .style("text-anchor", "end");
+
+    y.domain(
+      data.map(function (d) {
+        return d.group;
+      })
+    );
+    Yaxis.current.transition().duration(1000).call(d3.axisLeft(y));
+
+    u.enter()
+      .append("rect")
+      .merge(u)
+      .transition()
+      .duration(1000)
+      .attr("y", function (d) {
+        return y(d.group);
+      })
+      .attr("x", x(0))
+      .attr("height", y.bandwidth())
+      .attr("width", function (d) {
+        if (d.value == NaN) {
+          return 0;
+        }
+        return (width * d.value) / xmax;
+      })
+      .attr("fill", function (d) {
+        return color.current[d.group];
+      });
+
+    u.exit().remove();
+  }
+
+  const Update = React.useRef(update);
+
+  //* Effect hook
+  useEffect(() => {
+    if (!change.current) {
+      Object.keys(props.data).forEach(
+        (element) =>
+          (color.current[props.data[element].group] =
+            "hsl(" + Math.random() * 360 + ",90%,75%)")
+      );
+
+      const test = divRef.current;
+      d3.selectAll("svg").remove();
+      test.style.width = "100%";
+      test.style.height = "100%";
+      test.style.backgroundColor = "white";
+      test.style.borderRadius = "10px";
+      test.style.boxShadow = "0px 1px 3px -1.5px rgba(0,0,0,0.75)";
+      test.style.minHeight = "70vh";
+      test.width = test.offsetWidth;
+      test.height = test.offsetHeight;
+
+      var margin = { top: 50, right: 30, bottom: 50, left: 110 },
         width = test.width * 0.8,
-        height = test.height * 0.9
+        height = test.height * 0.9;
 
-        
-        var temp = data.sort(function(b,a){
-            return a.value - b.value
-        })
+      // append the svg object to the body of the page
+      var svg = d3
+        .select(test)
+        .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-        var xmax = d3.max(data, function(d) { return d.value })
-        
-        
-        var svg = d3.select(divRef.current)
-        var u = svg.selectAll("rect")
-        .data(temp)
-
-        // X axis
-        var x = d3.scaleLinear()
-            .domain([0, 10000])
-            .range([ 0, width]);
-        
-        // Y axis
-        var y = d3.scaleBand()
-            .domain(props.data.map((d)=>d.group))
-            .range([ 0, height ])
-            .padding(.1);
-        
-        x.domain([0,  xmax]);
-        Xaxis.current.transition().duration(1000).call(d3.axisBottom(x))
+      // X axis
+      var x = d3.scaleLinear().domain([0, 1000000]).range([0, width]);
+      var xaxis = svg.append("g");
+      xaxis
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(x))
         .selectAll("text")
         .attr("transform", "translate(-10,0)rotate(-45)")
         .style("text-anchor", "end");
-            
-        y.domain(data.map(function(d) { return d.group; }))
-        Yaxis.current.transition().duration(1000).call(d3.axisLeft(y));
-        
-    
-        u
-        .enter()
+      Xaxis.current = xaxis;
+
+      // Y axis
+      var y = d3
+        .scaleBand()
+        .domain(datadict.current.map((d) => d.group))
+        .range([0, height])
+        .padding(0.1);
+      var yaxis = svg.append("g").call(d3.axisLeft(y));
+
+      Yaxis.current = yaxis;
+
+      var u = svg.selectAll("rect").data(datadict.current);
+
+      u.enter()
         .append("rect")
-        .merge(u)
-        .transition()
-        .duration(1000)
-        .attr("y", function(d) { return y(d.group); })
         .attr("x", x(0))
+        .attr("y", function (d) {
+          return y(d.group);
+        })
+        .attr("width", function (d) {
+          return x(d.value);
+        })
         .attr("height", y.bandwidth())
-        .attr("width", function(d) {
-            if (d.value == NaN) {
-                return 0}
-            return width * d.value/xmax; })
-        .attr("fill", function(d){return color.current[d.group]})
-    
-        u
-        .exit()
-        .remove()
+        .attr("fill", function (d) {
+          return color.current[d.group];
+        });
+
+      Update.current(datadict.current);
+
+      change.current = true
+
     }
+    else
+    {
+        Update.current(props.data)
+    }
+  }, [props.data]);
 
-    const Update = React.useRef(update)
-
-    //* Effect hook
-    useEffect(() => {
-    Object.keys(props.data).forEach(
-        element =>
-        color.current[props.data[element].group] = "hsl(" + Math.random() * 360 + ",90%,75%)"
-    ) 
-
-    const test = divRef.current
-    d3.selectAll("svg").remove()
-    test.style.width = "100%";
-    test.style.height = "100%";
-    test.style.backgroundColor = "white";
-    test.style.borderRadius = "10px";
-    test.style.boxShadow = "0px 1px 3px -1.5px rgba(0,0,0,0.75)";
-    test.style.minHeight = "70vh";
-    test.width = test.offsetWidth;
-    test.height = test.offsetHeight;    
-
-    var margin = {top: 50, right: 30, bottom: 50, left: 110},
-    width = test.width * 0.8,
-    height = test.height * 0.9
-
-    // append the svg object to the body of the page
-    var svg = d3.select(test)
-    .append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-    .attr("transform",
-        "translate(" + margin.left + "," + margin.top + ")");
-
-    // X axis
-    var x = d3.scaleLinear()
-        .domain([0, 1000000])
-        .range([ 0, width]);
-    var xaxis = svg.append("g")
-    xaxis.attr("transform", "translate(0," + height + ")")
-    .call(d3.axisBottom(x))
-    .selectAll("text")
-    .attr("transform", "translate(-10,0)rotate(-45)")
-    .style("text-anchor", "end");
-    Xaxis.current = xaxis
-    
-
-    // Y axis
-    var y = d3.scaleBand()
-        .domain(datadict.current.map((d)=>d.group))
-        .range([ 0, height ])
-        .padding(.1);
-    var yaxis = svg.append("g")
-        .call(d3.axisLeft(y))
-    
-    Yaxis.current = yaxis
-
-
-    var u = svg.selectAll("rect").data(datadict.current)
-
-    u
-    .enter()
-    .append("rect")
-    .attr("x", x(0) )
-    .attr("y", function(d) { return y(d.group); })
-    .attr("width", function(d) { return x(d.value); })
-    .attr("height", y.bandwidth() )
-    .attr("fill", function(d){return color.current[d.group]})
- 
-    Update.current(datadict.current)
-
-    console.log('component updated')
-  
-    }, []);
-    
-
-    return (
-        <Container>
-            {/* <canvas id="myCanvas" ref={canvasRef} >
+  return (
+    <Container>
+      {/* <canvas id="myCanvas" ref={canvasRef} >
             </canvas> */}
-            <div id='test' ref={divRef}></div>
-            <Box sx={{ m: '0.5rem' }}>
-                <CoolSlider
-                    valueLabelDisplay="auto"
-                    defaultValue={0}
-                    max={Object.keys(props.date).length-1}
-                    scale={(value)=> props.date[value]}
-                    onChange={(value) => {
-                        props.handleSlide(value.target.value)
-                        props.changeDate(value.target.value)
-                        update(props.data)
-                    }}
-                />
-            </Box>
-            <ButtonRow />
-        </Container>
-    )
+      <div id="test" ref={divRef}></div>
+      <Box sx={{ m: "0.5rem" }}>
+        <CoolSlider
+          valueLabelDisplay="auto"
+          defaultValue={0}
+          max={Object.keys(props.date).length - 1}
+          scale={(value) => props.date[value]}
+          onChange={(value) => {
+            props.handleSlide(value.target.value);
+            props.changeDate(value.target.value);
+          }}
+        />
+      </Box>
+      <ButtonRow />
+    </Container>
+  );
 }
 
 /**
@@ -181,42 +195,42 @@ export default function Chart(props) {
  * https://mui.com/material-ui/api/slider/
  */
 const CoolSlider = styled(Slider)({
-    color: '#1976D2',
-    height: 8,
-    '& .MuiSlider-track': {
-        border: 'none',
+  color: "#1976D2",
+  height: 8,
+  "& .MuiSlider-track": {
+    border: "none",
+  },
+  "& .MuiSlider-thumb": {
+    height: 24,
+    width: 24,
+    backgroundColor: "#fff",
+    border: "2px solid currentColor",
+    "&:focus, &:hover, &.Mui-active, &.Mui-focusVisible": {
+      boxShadow: "inherit",
     },
-    '& .MuiSlider-thumb': {
-        height: 24,
-        width: 24,
-        backgroundColor: '#fff',
-        border: '2px solid currentColor',
-        '&:focus, &:hover, &.Mui-active, &.Mui-focusVisible': {
-        boxShadow: 'inherit',
-        },
-        '&:before': {
-        display: 'none',
-        },
+    "&:before": {
+      display: "none",
     },
-    '& .MuiSlider-valueLabel': {
-        lineHeight: 1.2,
-        fontSize: 12,
-        background: 'unset',
-        padding: 0,
-        width: 32,
-        height: 32,
-        borderRadius: '50% 50% 50% 0',
-        backgroundColor: '#1976D2',
-        transformOrigin: 'bottom left',
-        transform: 'translate(50%, -100%) rotate(-45deg) scale(0)',
-        '&:before': { display: 'none' },
-        '&.MuiSlider-valueLabelOpen': {
-        transform: 'translate(50%, -100%) rotate(-45deg) scale(1)',
-        },
-        '& > *': {
-        transform: 'rotate(45deg)',
-        },
+  },
+  "& .MuiSlider-valueLabel": {
+    lineHeight: 1.2,
+    fontSize: 12,
+    background: "unset",
+    padding: 0,
+    width: 32,
+    height: 32,
+    borderRadius: "50% 50% 50% 0",
+    backgroundColor: "#1976D2",
+    transformOrigin: "bottom left",
+    transform: "translate(50%, -100%) rotate(-45deg) scale(0)",
+    "&:before": { display: "none" },
+    "&.MuiSlider-valueLabelOpen": {
+      transform: "translate(50%, -100%) rotate(-45deg) scale(1)",
     },
+    "& > *": {
+      transform: "rotate(45deg)",
+    },
+  },
 });
 
 /**
@@ -224,28 +238,26 @@ const CoolSlider = styled(Slider)({
  * @todo: Make the buttons show the different charts
  */
 function ButtonRow() {
-    const [alignment, setAlignment] = React.useState('playerCount');
-  
-    const handleChange = (event, newAlignment) => {
-      setAlignment(newAlignment);
-    };
-  
-    return (
+  const [alignment, setAlignment] = React.useState("playerCount");
 
-        <ToggleButtonGroup
-            color="primary"
-            value={alignment}
-            exclusive
-            onChange={handleChange}
-            aria-label="Platform"
-            alignItems="center"
-            justifyContent="center"
-        >
-            <ToggleButton value="playerCount">Player Count</ToggleButton>
-            <ToggleButton value="playedTime">Played Time</ToggleButton>
-            <ToggleButton value="engagement">Engagement</ToggleButton>
-            <ToggleButton value="price">Price</ToggleButton>
-        </ToggleButtonGroup>
+  const handleChange = (event, newAlignment) => {
+    setAlignment(newAlignment);
+  };
 
-    );
-  }
+  return (
+    <ToggleButtonGroup
+      color="primary"
+      value={alignment}
+      exclusive
+      onChange={handleChange}
+      aria-label="Platform"
+      alignItems="center"
+      justifyContent="center"
+    >
+      <ToggleButton value="playerCount">Player Count</ToggleButton>
+      <ToggleButton value="playedTime">Played Time</ToggleButton>
+      <ToggleButton value="engagement">Engagement</ToggleButton>
+      <ToggleButton value="price">Price</ToggleButton>
+    </ToggleButtonGroup>
+  );
+}
